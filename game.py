ffcb5f72824eebde448b2d77234f9063fbc0b310
unknown <d1360n4v4rr0@gmail.com>
2022-09-30 16:20:34 -0600

    h65

--- game.py
+++ game.py
@@ -1,1 +1,13 @@
-n = 1237833
+import imp
+
+
+import pygame as pg
+
+pg.init()
+screen = pg.display.set_mode((800,400))
+
+while True:
+    for event in  pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+pg.display.update()
