2f75574c32f1fb71f553d34092e8f083a7de1070
unknown <d1360n4v4rr0@gmail.com>
2022-10-03 15:58:05 -0600

    added a surface

--- game.py
+++ game.py
@@ -8,10 +8,13 @@ pg.init()
 screen = pg.display.set_mode((800,400))
 pg.display.set_caption("Running")
 clock = pg.time.Clock()
+test_Surface = pg.Surface(100,200)
+test_Surface.fill('Red')
 while True:
     for event in  pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
 pg.display.update()
+screen.blit(test_Surface, (0,0))
 clock.tick(60) 
