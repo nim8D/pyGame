736bda6d52365b7bfc18fae4ad3e406550a4214a
unknown <d1360n4v4rr0@gmail.com>
2022-10-03 15:36:42 -0600

    added caption

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import pygame as pg
 
 pg.init()
 screen = pg.display.set_mode((800,400))
-
+pg.display.set_caption("Running")
 while True:
     for event in  pg.event.get():
         if event.type == pg.QUIT:
