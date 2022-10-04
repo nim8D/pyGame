3d418ab5dc9484163904634d5df98d716b0b62a5
unknown <d1360n4v4rr0@gmail.com>
2022-10-04 10:36:13 -0600

    added a surface 3

--- game.py
+++ game.py
@@ -8,13 +8,15 @@ pg.init()
 screen = pg.display.set_mode((800,400))
 pg.display.set_caption("Running")
 clock = pg.time.Clock()
-test_Surface = pg.Surface((100,200))
-test_Surface.fill('Red')
+test_Surface = pg.image.load('graphics/Sky.png')
+ground_Surface = pg.image.load('graphics/ground.png')
+
 while True:
     for event in  pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
         screen.blit(test_Surface, (0,0))
+        screen.blit(ground_Surface, (0,100))
         pg.display.update()
         clock.tick(60) 
